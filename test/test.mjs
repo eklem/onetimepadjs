592cab0c8b7432be20ef8b9f5c5cacd5f482f5ad
Espen Klem <espen.klem@gmail.com>
2022-04-08 12:36:36 +0200

    Cleaning up test

--- test/test.mjs
+++ test/test.mjs
@@ -1,19 +1,12 @@
 import { textToPlaincode, plaincodeToText, createOnetimePad, nob, codebook, checkLength, encryptPlaincode, decryptEncryptedMsg } from '../src/index.mjs'
 
-const plaincode = '72162626399022530316200000034229999806326264999019029039979767394'
 const txt = 'Hello 👨‍👩‍👦‍👦🏳️‍🌈😀🇿🇼  world 123 æøå!'
 
-console.log('\n\nInput:               ' + txt)
+console.log('\n\nMessage:             ' + txt)
 
 // ### Testing: Text to plaincode
 const plaincodeConverted = textToPlaincode(txt, nob, codebook)
 console.log('Plaincode:           ' + plaincodeConverted)
-
-if (plaincodeConverted === plaincode) {
-  // console.log('\n### textToPlaincode works!\n')
-} else {
-  console.log('\n### textToPlaincode doesn\'t work!\n')
-}
 
 // ### Testing: Creating a one-time pad
 const otp = createOnetimePad(100)
@@ -32,10 +25,4 @@ console.log('Decrypted plaincode: ' + decryptedPlaincode.join(''))
 
 // ### Testing: Plaincode to text
 const textConverted = plaincodeToText(decryptedPlaincode.join(''), nob, codebook)
-console.log('Decrypted msg:       ' + textConverted + '\n\n')
-
-if (textConverted === txt.toLowerCase()) {
-  // console.log('\n### plaincodeToText works!\n')
-} else {
-  console.log('\n### plaincodeToText doesn\'t work!\n')
-}
+console.log('Decrypted message:   ' + textConverted + '\n\n')
